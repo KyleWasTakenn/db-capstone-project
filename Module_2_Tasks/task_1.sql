e31f2cd762d8ffeefce68584beb2fb461f99f7e6
KyleWasTakenn <kylebillheimer@gmail.com>
2025-06-14 19:13:09 -0700

    Added task 1 script and executed it in the Database.

---
+++ Module_2_Tasks/task_1.sql
@@ -0,0 +1,22 @@
+START TRANSACTION;
+
+CREATE VIEW OrdersView AS
+-- Selects Orders.OrderID, OrderItems Quantity, and calculates total cost)
+SELECT
+     o.OrderID AS "Order ID",
+     SUM(oi.Quantity) AS "No. of items",
+     SUM(oi.Quantity * mi.ItemCost) AS "Total Cost"
+FROM
+	Orders o
+JOIN
+	OrderItems oi ON o.OrderID = oi.OrderID
+JOIN
+	MenuItems mi ON oi.ItemID = mi.ItemID
+GROUP BY o.OrderID
+HAVING SUM(oi.Quantity) > 2;
+
+-- OrderItems is essentially Shopping Cart.
+
+-- COMMIT OR ROLLBACK (Don't think it's needed but just in case)
+-- COMMIT;
+-- ROLLBACK;
