63297c1b7956a01d8c942ddc2d96505c74ea4503
KyleWasTakenn <kylebillheimer@gmail.com>
2025-06-14 19:39:01 -0700

    Added query for task 2

---
+++ Module_2_Tasks/task_2.sql
@@ -0,0 +1,30 @@
+SELECT
+	c.CustomerID AS "Customer ID",
+	CONCAT(c.CustomerFirstName, " ", c.CustomerLastName) AS "Full Name",
+	o.OrderID AS "Order ID",
+	SUM(mi.ItemCost * oi.Quantity) AS "Order Cost",
+	m.MenuName AS "Menu Name",
+	mi.CourseName AS "Course Name",
+	mi.StarterName AS "Starter Name"
+FROM
+	Customers c
+JOIN
+	Orders o ON  o.OrderCustomerID = c.CustomerID
+JOIN
+	OrderItems oi ON o.OrderID = oi.OrderID
+JOIN
+	MenuItems mi ON oi.ItemID = mi.ItemID
+JOIN
+	Menus m ON mi.ItemID = m.MenuItemID
+GROUP BY
+	c.CustomerID,
+    c.CustomerFirstName,
+    c.CustomerLastName,
+    o.OrderID,
+    m.MenuName,
+    mi.CourseName,
+    mi.StarterName
+HAVING SUM(mi.ItemCost * oi.Quantity) > 150;
+
+-- In this dataset, the expected result from this is 0 customers.
+-- No customers have spent > $150 in my data.
