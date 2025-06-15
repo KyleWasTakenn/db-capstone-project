ab948797e96ce8a4d701cd0510cf429307c2c2c0
KyleWasTakenn <kylebillheimer@gmail.com>
2025-06-14 21:37:54 -0700

    - Added comment in task 4 script explaining the task.

--- Module_2_Tasks/task_4.sql
+++ Module_2_Tasks/task_4.sql
@@ -1,3 +1,10 @@
+/*
+In this task, Little Lemon need you to create a procedure that:
+displays the maximum ordered quantity in the Orders table.
+
+In this case, this comes from the OrderItems table because of my
+database Schema.
+*/
 DELIMITER //
 CREATE PROCEDURE GetMaxQuantity()
 BEGIN
